461978d3b400b55bbe26533e3e0f83d5ff75440c
christianborn <chrisborn@hotmail.de>
2022-09-28 16:40:11 +0200

    Add more Tests and finish implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println(calculateFakultaet(5));
+    }
     public static int calculateFakultaet(int number) {
-        return number;
+        if (number == 0) {
+            return number;
+        }
+        int result = 1;
+        for(int i = 1; i <=number; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void fakultaet0 () {
+        // Given
+        int number = 0;
+        // When
+        int actual = Main.calculateFakultaet(number);
+        // Then
+        int expectation = 0;
+        assertEquals(expectation, actual);
+    }
+    @Test
+    public void fakultaet1 () {
+        // Given
+        int number = 1;
+        // When
+        int actual = Main.calculateFakultaet(number);
+        // Then
+        int expectation = 1;
+        assertEquals(expectation, actual);
+    }
+    @Test
+    public void fakultaet2 () {
+        // Given
+        int number = 2;
+        // When
+        int actual = Main.calculateFakultaet(number);
+        // Then
+        int expectation = 2;
+        assertEquals(expectation, actual);
+    }
+    @Test
+    public void fakultaet3 () {
+        // Given
+        int number = 3;
+        // When
+        int actual = Main.calculateFakultaet(number);
+        // Then
+        int expectation = 6;
+        assertEquals(expectation, actual);
+    }
+}
